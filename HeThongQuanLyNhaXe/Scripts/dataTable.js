980acf925de5c7f6c567309d99fa38f8ea164c40
DuongNX <duongnx.work@gmail.com>
2019-03-01 17:26:44 +0700

    Add Project

---
+++ HeThongQuanLyNhaXe/Scripts/dataTable.js
@@ -0,0 +1,33 @@
+﻿jQuery(function ($) {
+    var myTable = $('#dynamic-table').DataTable({
+        pageLength: 10,
+        paging: true,
+        searching: true,
+        order: [[0, "asc"]],
+        columnDefs: [{ orderable: false, targets: [2] }] // 6 was the highest index, not 7
+    });
+
+    new $.fn.dataTable.Buttons(myTable, {
+        buttons: [
+            {
+                "extend": "csv",
+                "text": "<i class='fa fa-database bigger-110 orange'></i> <span class='hidden'>Export to CSV</span>",
+                "className": "btn btn-white btn-primary btn-bold"
+            },
+            {
+                "extend": "print",
+                "text": "<i class='fa fa-print bigger-110 grey'></i> <span class='hidden'>Print</span>",
+                "className": "btn btn-white btn-primary btn-bold",
+                autoPrint: false
+            }
+        ]
+    });
+    myTable.buttons().container().appendTo($('.tableTools-container'));
+
+    $(document).on('click', '#dynamic-table .dropdown-toggle', function (e) {
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+        e.preventDefault();
+    });
+
+})
